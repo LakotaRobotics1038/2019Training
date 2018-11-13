640db7e9d0439454d2419577c8dc6fdd8998b11a
Courtney Lyden <lydencourtney@gmail.com>
2018-11-12 19:54:59 -0500

    I worked on PIDs

--- src/org/usfirst/frc/team1038/robot/RobotDriveCode.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team1038.robot;
-
-public class RobotDriveCode {
-
-}
---
+++ src/org/usfirst/frc/team1038/robot/RobotDriveTrain.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team1038.robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class RobotDriveTrain extends Subsystem {
+	
+	public RobotDriveTrain() {
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+}
