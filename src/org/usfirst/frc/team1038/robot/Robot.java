a67f5463a2f11164ff73be2fc16fb33ec6432d95
Courtney Lyden <lydencourtney@gmail.com>
2018-12-01 11:46:45 -0500

    did basically nothing

--- src/org/usfirst/frc/team1038/robot/Robot.java
+++ src/org/usfirst/frc/team1038/robot/Robot.java
@@ -10,9 +10,6 @@ package org.usfirst.frc.team1038.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.Scheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,75 +19,68 @@ import edu.wpi.first.wpilibj.command.Scheduler;
  * project.
  */
 public class Robot extends IterativeRobot {
-	
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
-	static RobotSparkMotor emptySpark = new RobotSparkMotor(9);
-	public static RobotDriveTrain driveTrain = RobotDriveTrain.getInstance();
-	RobotEncoder firstEncoder = RobotEncoder.getInstance();
-	RobotEncoder secondEncoder = RobotEncoder.getFirstInstance();
-	RobotSparkMotor firstMotor = RobotSparkMotor.getInstance();
-	RobotSparkMotor secondMotor = RobotSparkMotor.getFirstInstance();
-	RobotSparkMotor thirdMotor = RobotSparkMotor.getSecondInstance();
-	JoystickCourtney firstJoystick = new JoystickCourtney(0);
-	Compressor airCompressor = new Compressor(0);
-	Scheduler schedule = Scheduler.getInstance();
-	
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
 	public void robotInit() {
-		airCompressor.setClosedLoopControl(true);
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
 	}
-	
-	public void teleopInit() {
-		schedule = null;
-		
-	}
-	
-	public void teleopPeriodic() {
-		firstMotor.set(firstJoystick.getLeftJoystickVertical() * .5);
-		secondMotor.set(firstJoystick.getLeftJoystickVertical() * .5);
-		thirdMotor.set(firstJoystick.getRightJoystickVertical() * .5);
-		System.out.println(firstJoystick.getLeftJoystickVertical() * .5);
-		System.out.println(firstEncoder.getCount() + " , " + secondEncoder.getCount());
-		if(firstJoystick.getXButton()) {
-			(RobotPneumatics.getFirstInstance()).highGear();
-		}
-		if(firstJoystick.getYButton()) {
-			(RobotPneumatics.getFirstInstance()).lowGear();
-		}
-		if(firstJoystick.getAButton()) {
-			(RobotPneumatics.getInstance()).PTOOn();
-		}
-		if(firstJoystick.getBButton()) {
-			(RobotPneumatics.getInstance()).PTOOff();
-		}
-	}
-	
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
 	public void autonomousInit() {
-		schedule.add(new RobotDriveStraight(2));
+		m_autoSelected = m_chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
 	}
-	
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
 	public void autonomousPeriodic() {
-		System.out.println(firstEncoder.getCount() + " , " + secondEncoder.getCount());
-		System.out.println(firstEncoder.getDistance() + " , " + secondEncoder.getDistance());
-		//to run motor for a distance:
-		/*
-		if(firstEncoder.getDistance() < 12) {
-			firstMotor.set(0.7);
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
 		}
-		else {
-			firstMotor.set(0);
-		}
-		if(secondEncoder.getDistance() < 12) {
-			secondMotor.set(-0.7);
-		}
-		else {
-			secondMotor.set(0);
-		}
-		*/
-		if (schedule != null) {
-			schedule.run();
-		}
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
 	}
 }
